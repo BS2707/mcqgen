a8279f65cd1b19e70217aa2abdf0f196e4d4ff09
Bhargavi Seelam <bhargavi.seelam@tui.co.uk>
2025-02-26 16:38:52 +0530

    Files Updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 #install local pkg in Virtual env
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='bhargavi seelam',
+    author_email='bhargavi.seelam27@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
